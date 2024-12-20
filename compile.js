f9ff4413020cae2646a0ff9b6b927a57e5affa58
Mohansingh12 <96055565+Mohansingh12@users.noreply.github.com>
2024-12-20 23:06:26 +0530

    day 3

--- compile.js
+++ compile.js
@@ -2,6 +2,30 @@ const path =require('path');
 const fs = require('fs');
 const solc=require('solc');
 
-const inboxpath=path.resolve(__dirname,'contract','contract.sol');
+const inboxpath=path.resolve(__dirname,'contract','Inbox.sol');
 const source=fs.readFileSync(inboxpath,'utf8');
 console.log(solc.compile(source,1));
+
+const input = {
+    language: 'Solidity',
+    sources: {
+      'Inbox.sol': {
+        content: source,
+      },
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['abi', 'evm.bytecode'],
+        },
+      },
+    },
+  };
+  
+  try {
+    const output = JSON.parse(solc.compile(JSON.stringify(input)));
+  
+    module.exports = output.contracts['Inbox.sol'];
+  } catch (err) {
+    console.error('Compilation failed:', err);
+  }
